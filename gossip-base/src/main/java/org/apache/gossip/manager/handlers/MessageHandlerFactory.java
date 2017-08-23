897d9589403735fed37c5a63166b7b5b0e58ad92
pxsalehi <pxsalehi@gmail.com>
2017-08-23 13:26:57 +0200

    applied code style

--- gossip-base/src/main/java/org/apache/gossip/manager/handlers/MessageHandlerFactory.java
+++ gossip-base/src/main/java/org/apache/gossip/manager/handlers/MessageHandlerFactory.java
@@ -25,27 +25,27 @@ import org.apache.gossip.model.*;
 import java.util.Arrays;
 
 public class MessageHandlerFactory {
-  
+
   public static MessageHandler defaultHandler() {
-    return concurrentHandler(
-        new TypedMessageHandler(Response.class, new ResponseHandler()),
-        new TypedMessageHandler(ShutdownMessage.class, new ShutdownMessageHandler()),
-        new TypedMessageHandler(PerNodeDataBulkMessage.class, new PerNodeDataMessageHandler()),
-        new TypedMessageHandler(SharedDataBulkMessage.class, new SharedDataMessageHandler()),
-        new TypedMessageHandler(ActiveGossipMessage.class, new ActiveGossipMessageHandler())
-    );
+    return concurrentHandler(new TypedMessageHandler(Response.class, new ResponseHandler()),
+            new TypedMessageHandler(ShutdownMessage.class, new ShutdownMessageHandler()),
+            new TypedMessageHandler(PerNodeDataBulkMessage.class, new PerNodeDataMessageHandler()),
+            new TypedMessageHandler(SharedDataBulkMessage.class, new SharedDataMessageHandler()),
+            new TypedMessageHandler(ActiveGossipMessage.class, new ActiveGossipMessageHandler()));
   }
-  
+
   public static MessageHandler concurrentHandler(MessageHandler... handlers) {
-    if (handlers == null) throw new NullPointerException("handlers cannot be null");
+    if (handlers == null)
+      throw new NullPointerException("handlers cannot be null");
     if (Arrays.asList(handlers).stream().filter(i -> i != null).count() != handlers.length) {
       throw new NullPointerException("found at least one null handler");
     }
     return new MessageHandler() {
-      @Override
-      public boolean invoke(GossipCore gossipCore, GossipManager gossipManager, Base base) {
+      @Override public boolean invoke(GossipCore gossipCore, GossipManager gossipManager,
+              Base base) {
         // return true if at least one of the component handlers return true.
-        return Arrays.asList(handlers).stream().filter((mi) -> mi.invoke(gossipCore, gossipManager, base)).count() > 0;
+        return Arrays.asList(handlers).stream()
+                .filter((mi) -> mi.invoke(gossipCore, gossipManager, base)).count() > 0;
       }
     };
   }
